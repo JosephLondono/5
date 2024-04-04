267e9019195f50611f3d6e04e70c9a9efb760796
JosephLondono <londonoj888@gmail.com>
2024-04-04 15:25:13 -0500

    Add Comments and SSR

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,23 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
 
 // https://astro.build/db/config
+const Authors = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true , autoIncrement: true }),
+    name: column.text(),
+  }
+})
+
+const Comments = defineTable({
+  columns: { 
+    title: column.text(),
+    description: column.text(),
+    completed: column.boolean(),
+    authorId: column.number({ references: () => Authors.columns.id }),
+  }
+})
+
+
 export default defineDb({
-  tables: {}
+  tables: { Authors , Comments }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,36 @@
-import { db } from 'astro:db';
+import { db, Authors, Comments } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+
+	await db.insert(Authors).values([
+		{
+			name: 'Alice',
+		},
+		{
+			name: 'Bob',
+		}
+	])
+
+	await db.insert(Comments).values([
+		{
+			title: "Buy milk",
+			description: "Buy milk from the store",
+			completed: false,
+			authorId: 1
+		},
+		{
+			title: "Buy eggs",
+			description: "Buy eggs from the store",
+			completed: false,
+			authorId: 1
+		},
+		{
+			title: "Buy bread",
+			description: "Buy bread from the store",
+			completed: false,
+			authorId: 2
+		}
+	])
 }
